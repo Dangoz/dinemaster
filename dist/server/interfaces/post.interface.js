a95cc2a967628c418c5756f13225a08dd1905839
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-05-17 17:40:17 -0700

    remove dist

--- dist/server/interfaces/post.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// import IComment from "./comment.interface";
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/src/components/search/search.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Search = function () {
-    return (<div>
-      
-    </div>);
-};
-exports.default = Search;
