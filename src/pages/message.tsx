6d97abf2ec28302c187b6c5dc4bd8e82671febae
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-05-20 18:23:55 -0700

    message page with swiper

--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -3,6 +3,8 @@ import Menu from "../components/menu";
 import { useEffect, useState, useRef } from "react";
 import { requireAuthen } from "../api/require.authen";
 import Content from "../components/home/content";
+import MessageStyle from "../styles/message.module.css";
+import Swiper from "../components/message/swiper";
 
 const message = ({ user }) => {
 
@@ -11,7 +13,32 @@ const message = ({ user }) => {
 
   return (
     <>
-      <Content userId={user.id}/>
+      <div className={MessageStyle.wrapper}>
+        <h2 className={MessageStyle.title}>Messages</h2>
+
+        <input className={MessageStyle.search} type="text" placeholder="Search"></input>
+
+        <div className={MessageStyle.closeFri}>
+          <div className={MessageStyle.closeFriText}>Close Friends</div>
+          <Swiper userId={user.id}/>
+        </div>
+
+        <div className={MessageStyle.item}>
+          <div className={MessageStyle.itemImg}>img</div>
+          <div className={MessageStyle.itemName}>Name</div>
+          <div className={MessageStyle.itemTime}>Time</div>
+        </div>
+        <div className={MessageStyle.item}>
+          <div className={MessageStyle.itemImg}>img</div>
+          <div className={MessageStyle.itemName}>Name</div>
+          <div className={MessageStyle.itemTime}>Time</div>
+        </div>
+        <div className={MessageStyle.item}>
+          <div className={MessageStyle.itemImg}>img</div>
+          <div className={MessageStyle.itemName}>Name</div>
+          <div className={MessageStyle.itemTime}>Time</div>
+        </div>
+      </div>
     
       <Menu />
     </>
