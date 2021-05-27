398d1da54d61abfcecc2f4b65a0ec5559941916f
Justin <71615891+whitebread778@users.noreply.github.com>
2021-05-27 13:44:23 -0700

    menu bar on click

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -16,11 +16,11 @@ const Menu = () => {
     <div className={MenuStyle.wrapper}>
 
       <ul className={MenuStyle.options}>
-        <li><Link href="/home"><img src="/menu/home.png" alt="home"></img></Link></li>
-        <li><Link href="/search/"><img src="/menu/search.png" alt="search"></img></Link></li>
-        <li><Link href="/post"><img className={MenuStyle.plus} src="/menu/plus.png" alt="plus"></img></Link></li>
-        <li><Link href="/message"><img className={MenuStyle.message} src="/menu/message.png" alt="message"></img></Link></li>
-        <li><Link href="/profile"><img src={path.includes("/profile") ? "/menu/profile.png" : "/menu/profile.png"} alt="profile"></img></Link></li>
+        <li><Link href="/home"><img src={path.includes("/home") ? "/menu/homeB.png" : "/menu/home.png"} alt="home"></img></Link></li>
+        <li><Link href="/search/"><img src={path.includes("/search") ? "/menu/searchB.png" : "/menu/search.png"} alt="search"></img></Link></li>
+        <li><Link href="/post"><img className={MenuStyle.plus} src={path.includes("/post") ? "/menu/plusB.png" : "/menu/plus.png"} alt="plus"></img></Link></li>
+        <li><Link href="/message"><img className={MenuStyle.message} src={path.includes("/message") ? "/menu/messageB.png" : "/menu/message.png"} alt="message"></img></Link></li>
+        <li><Link href="/profile"><img src={path.includes("/profile") ? "/menu/profileB.png" : "/menu/profile.png"} alt="profile"></img></Link></li>
       </ul>
 
     </div>
