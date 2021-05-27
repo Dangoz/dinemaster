53583fd1c4ab4d3116fc0d32b1a9141f84ca5118
Justin <71615891+whitebread778@users.noreply.github.com>
2021-05-27 15:10:45 -0700

    I don't know

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -6,19 +6,37 @@ import { useState, useEffect, useRef } from "react";
 import Post from "../api/post";
 
 const post = ({ user }) => {
+  const [isPrompt, setIsPrompt] = useState(false);
+  const [isReset, setIsReset] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [preview, setPreview] = useState('/logo_black_vertical.svg');
   const [caption, setCaption] = useState('');
   const [file, setFile] = useState(null);
   const [tags, setTags] = useState([]);
   const uploadRef = useRef(null);
- 
+
   const createPost = async (event) => {
     console.log('creating post! with: ' + tags)
+    setIsLoading(true);
     event.preventDefault();
     const data = { caption, file, tags }
     await Post.createImagePost(data, user)
       .then(response => {
-        
+        console.log(response.status);
+
+        setIsPrompt(true);
+        setTimeout(() => {
+          setIsPrompt(false);
+        }, 1200)
+
+        setIsReset(true);
+        setTags([]);
+        setFile(null);
+        setCaption('');
+        setPreview('/logo_black_vertical.svg');
+
+        setIsLoading(false);
       })
   }
 
@@ -48,24 +66,35 @@ const post = ({ user }) => {
     uploadRef.current.click();
   }
 
+  useEffect(() => {
+    if (file) return setIsEmpty(false);
+    setIsEmpty(true);
+  }, [file])
+
   return (
     <>
+      <img className={isPrompt ? PostStyle.promptShow : PostStyle.prompt} src={'/prompt.png'} />
       <div className={PostStyle.window}>
         <div className={PostStyle.wrapper}>
           <h2 className={PostStyle.title}>New Post</h2>
           <form className={PostStyle.formWrapper} onSubmit={e => e.preventDefault()}>
             <div className={PostStyle.postArea}>
-              <img className={PostStyle.preview} src={preview}/>
+              <img className={PostStyle.preview} src={preview} />
 
               <button className={PostStyle.greenChoose} type="submit" value="Upload" onMouseDown={upload}>Upload Picture</button>
-              <input className={PostStyle.choosePic} type="file" accept="image/*" name="image" onChange={fileChange} ref={element => uploadRef.current = element}/>
-              <textarea className={PostStyle.content} name="message" placeholder="Enter content here..." onChange={captionChange} value={caption} maxLength={200}/>
+              <input className={PostStyle.choosePic} type="file" accept="image/*" name="image" onChange={fileChange} ref={element => uploadRef.current = element} />
+              <textarea className={PostStyle.content} name="message" placeholder="Enter content here..." onChange={captionChange} value={caption} maxLength={200} />
               <div className={PostStyle.contentCount}>{`${caption ? caption.length : 0}/200`}</div>
             </div>
 
-            <TagForm pushTag={pushTag} removeTag={removeTag}/>
+            <TagForm pushTag={pushTag} removeTag={removeTag} isReset={isReset} />
 
-            <button className={PostStyle.greenBtn} type="submit" value="Create" onMouseDown={createPost}>Create</button>
+            {isLoading
+              ? <button className={PostStyle.greenBtnLoader} >Processing...</button>
+              : isEmpty
+                ? <button className={PostStyle.greenBtnEmpty}>Upload Required</button>
+                : <button className={PostStyle.greenBtn} type="submit" value="Create" onMouseDown={createPost}>Create</button>}
+
           </form>
         </div>
       </div>
